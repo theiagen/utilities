7cfefb5bf5994b3c3bcec82eaaa5e68987d7212c
frankambrosio3 <frank.ambrosio@theiagen.com>
2021-12-29 10:12:08 -0500

    split all zipcodes on '-' delimiter and map county based on just the first half if it exists

--- scripts/ns3_metadata_cleanser.py
+++ scripts/ns3_metadata_cleanser.py
@@ -19,36 +19,29 @@ def get_opts():
 	return args
 arguments = get_opts()
 
-# read in metadata csv file
+# read in metadata csv file AND CONVERT ALL ZIPCODES TO STRINGS
 meta_csv1 = arguments.csv_meta_file
-meta_df1 = pd.read_csv(meta_csv1, dtype={'zip': str}, error_bad_lines=False, index_col=False)
+meta_df1 = pd.read_csv(meta_csv1, dtype={'zip': str})
 
+# read in ZIPCODE csv file AND CONVERT ALL ZIPCODES TO STRINGS
 zip_csv1 = arguments.county_zipcodes_file
 zip_df1 = pd.read_csv(zip_csv1, dtype={'ZipCode': str})
+
+meta_df1[['zip1','zip2']] = meta_df1['zip'].str.split('-',expand=True)
+
+
 
 # make a dictionary for fast zip/county lookups
 zip_county_lookup_dict = dict(zip(zip_df1.ZipCode, zip_df1.County))
 
-# add new column 'county' mapped from zip1
-meta_df1['county'] = meta_df1['zip'].map(zip_county_lookup_dict)
+# add new column 'county' mapped from zip
+meta_df1['county'] = meta_df1['zip1'].map(zip_county_lookup_dict)
 
-# input_headers = meta_df1.columns.values
+# list of headers to be included in the final output file
 output_headers = ['entity:cdc_specimen_id', 'collection_date', 'county', 'gisaid_accession', 'nextclade_clade', 'pango_lineage', 'sequencing_lab', 'state', 'zip']
 
 # rename headers
 meta_df1.rename(columns={'vendor_accession': 'entity:cdc_specimen_id', 'GISAID_accession': 'gisaid_accession', 'clade_Nextclade_clade': 'nextclade_clade', 'lineage_PANGO_lineage': 'pango_lineage', 'vendor': 'sequencing_lab'}, inplace=True)
-
-# replace all
-
-
-# for i in meta_df1['county']:
-# 	if re.match('*-*', i):
-# 		i = i.split('-')[0]
-#
-# meta_df1 = pd.to_numeric(meta_df1['county'])
-
-
-
 
 # drop extraneous cols
 drop_list = []
@@ -81,5 +74,5 @@ out_file_name = arguments.out_file
 # Print to tsv file
 meta_file_out = meta_df1.to_csv(out_file_name, sep="\t", index=False)
 
-#print to stdout
-# print(zip_county_lookup_dict)
+# print to stdout
+print(meta_df1)
