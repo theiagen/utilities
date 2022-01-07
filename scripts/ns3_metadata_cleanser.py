a447d979b2c591a480e05ba12b239065c444cbaa
frankambrosio3 <frank.ambrosio@theiagen.com>
2022-01-07 12:36:50 -0500

    Added flags for inputs, added a -r flag to remove rows with a blank in the gisaid_accession column

--- scripts/ns3_metadata_cleanser.py
+++ scripts/ns3_metadata_cleanser.py
@@ -9,15 +9,30 @@ import re
 #argpase used to take in command line arguments
 # three positional arguments, argparse might be overkill, sys command included
 def get_opts():
-	p = argparse.ArgumentParser(description = 'This program reads in a csv of sequence metadata and performs some reformatting and data sanitization then spits out a tsv to be uploaded to terra.bio', usage='[-h] metadata_cleanser.py <metadata_file.csv> <target_terra_data_table_name> <ZipCode_County_Lookup_Table> <outfile_name>')
-	p.add_argument('csv_meta_file',
+	p = argparse.ArgumentParser(description = 'This program reads in a csv of sequence metadata and performs some reformatting and data sanitization then spits out a tsv to be uploaded to terra.bio',
+								usage='[-h] metadata_cleanser.py <metadata_file.csv> <target_terra_data_table_name> <ZipCode_County_Lookup_Table> <outfile_name>')
+	p.add_argument('-i',
+				'--input_csv',
+				dest='csv_meta_file',
 				help='csv metadata file input')
-	p.add_argument('root_entity',
+	p.add_argument('-e',
+				'--entity',
+				dest='root_entity',
 				help='name of the Terra data table to which the metadata will be added (no _id required)')
-	p.add_argument('county_zipcodes_file',
+	p.add_argument('-z',
+				'--zipcodes',
+				dest='county_zipcodes_file',
 				help='csv file containing columns mapping zipcodes to county')
-	p.add_argument('out_file',
+	p.add_argument('-o',
+				'--output',
+				default='ns3_metadata_blessed.tsv',
+				dest='out_file',
 				help='Output file: required, must be a string.')
+	p.add_argument('-r',
+				'--remove',
+				action='store_true',
+				dest='remove_blanks_bool',
+				help='This flag will turn on or off the removal of rows with a blank value in the gisaid_accession column.')
 	args = p.parse_args()
 	return args
 arguments = get_opts()
@@ -50,8 +65,13 @@ output_headers = ['entity:{}_id'.format(root_entity_name1), 'collection_date', '
 # rename headers
 meta_df1.rename(columns={'vendor_accession': 'entity:{}_id'.format(root_entity_name1), 'GISAID_accession': 'gisaid_accession', 'clade_Nextclade_clade': 'nextclade_clade', 'lineage_PANGO_lineage': 'pango_lineage', 'vendor': 'sequencing_lab'}, inplace=True)
 
-# drop rows with NA in the gisaid_epi_isl column
-# meta_df1.dropna(subset=['gisaid_accession'], inplace=True)
+# drop rows with NA in the gisaid_epi_isl column if -r (--remove) flag is given
+rm_lines_w_blank_gisaid_accession = arguments.remove_blanks_bool
+if rm_lines_w_blank_gisaid_accession==True:
+	print('Rows with no gisaid_accession WILL be removed from the output.')
+	meta_df1.dropna(subset=['gisaid_accession'], inplace=True)
+else:
+	print('Rows with no gisaid_accession will NOT be removed from the output.')
 
 # drop extraneous cols
 drop_list = []
