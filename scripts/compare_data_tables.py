56eb510857d7dd82f6729b652762a02244d3ba43
Robert A. Petit III <robbie.petit@gmail.com>
2022-01-19 21:03:41 +0000

    fix white space

--- scripts/compare_data_tables.py
+++ scripts/compare_data_tables.py
@@ -7,15 +7,15 @@ import pandas as pd
 #argpase used to take in command line arguments
 # three positional arguments, argparse might be overkill, sys command included
 def get_opts():
-	p = argparse.ArgumentParser(description = 'This file compares two tsv files and reports the significant differences', usage='[-h] compare_data_tables.py <table_1.tsv> <table_2.tsv> <outfile_name>')
-	p.add_argument('tsv_file_1',
-				help='first tsv file input')
-  p.add_argument('tsv_file_2',
+    p = argparse.ArgumentParser(description = 'This file compares two tsv files and reports the significant differences', usage='[-h] compare_data_tables.py <table_1.tsv> <table_2.tsv> <outfile_name>')
+    p.add_argument('tsv_file_1',
+                help='first tsv file input')
+    p.add_argument('tsv_file_2',
                 help='second tsv file input')
-	p.add_argument('out_file',
-				help='Output file: required, must be a string.')
-	args = p.parse_args()
-	return args
+    p.add_argument('out_file',
+                help='Output file: required, must be a string.')
+    args = p.parse_args()
+    return args
 arguments = get_opts()
 tsv1 = arguments.tsv_file_1
 tsv2 = arguments.tsv_file_2
