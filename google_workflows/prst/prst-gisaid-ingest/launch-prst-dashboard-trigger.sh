ddbee3d132f781ac634e1a9321211764947c3b09
kapsakcj <kapsakcj@gmail.com>
2024-03-13 16:38:51 -0400

    update script for launching prst-gisaid-ingest trigger to use terra-batchie-watchie service account and update name of trigger and workflow; also updated location

--- google_workflows/prst/prst-gisaid-ingest/launch-prst-dashboard-trigger.sh
+++ google_workflows/prst/prst-gisaid-ingest/launch-prst-dashboard-trigger.sh
@@ -1,7 +1,7 @@
-gcloud eventarc triggers create prst-gisaid-trigger-standard \
-	--destination-workflow=prst-dashboard-standard \
+gcloud eventarc triggers create prst-gisaid-ingest-trigger \
+	--destination-workflow=prst-gisaid-ingest \
 	--destination-workflow-location=us-central1 \
 	--event-filters="type=google.cloud.storage.object.v1.finalized" \
 	--event-filters="bucket=pr-dashboard-gisaid-data" \
-	--location=us \
-	--service-account="932099266299-compute@developer.gserviceaccount.com"
+	--location=us-central1 \
+	--service-account="terra-batchie-watchie@general-theiagen.iam.gserviceaccount.com"
